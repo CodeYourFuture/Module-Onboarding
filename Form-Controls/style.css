0e08e4d2956a4833c8548418c2b025dd0b49434c
Durga <durga.weliwitegoda@gmail.com>
2025-05-22 10:44:18 +0100

    Create style.css

---
+++ Form-Controls/style.css
@@ -0,0 +1,39 @@
+body {
+  background-color: lightgrey;
+  padding: 50px;
+  border: 4px solid black;
+}
+
+input[type=submit], select{
+  width:100%;
+  background-color:grey;
+  color:white;
+  padding:14px 20px;
+  margin: 8px 0;
+  border:none;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+input[type=text], select{
+  width:100%;
+  background-color:white;
+  color:black;
+  padding:10px 20px;
+  margin: 8px 0;
+  border:10px;
+  border-radius: 4px;
+}
+
+input[type=email], select{
+  width:100%;
+  background-color:white;
+  color:black;
+  padding:10px 20px;
+  margin: 8px 0;
+  border:10px;
+  border-radius: 4px;
+}
+
+
+
